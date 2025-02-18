9164a5a80166907bc8e58d6c728bd86e3a21f90a
ksdkaveesha <ksdkaveesha@gmail.com>
2025-02-18 22:24:08 +0530

    Update Landing.jsx

--- src/Components/Dashboard/Landing.jsx
+++ src/Components/Dashboard/Landing.jsx
@@ -15,10 +15,65 @@ export default function Landing() {
     return (
         <div>
             <div className="flex flex-col md:flex-row items-center bg-transparent min-h-1/2 m-0 w-full p-0">
-                {/* Main content section */}
+                {/* Main content section MOH*/}
+                {/* <div className="md:w-1/2 p-12 text-center md:text-left" id="MOH">
+                    <div>
+                        <div>
+                            <div className='flex flex-col md:flex-row items-center text-center'>
+                                <button
+                                    onClick=''
+                                    className='md:m-4 m-4 rounded-3xl border-2 bg-emerald-500 border-emerald-600 md:w-full w-11/12 h-30 shadow-2xl transition duration-700 ease-in-out hover:scale-105'
+                                >
+                                    <div className='flex-shrink-0 flex items-center justify-center p-3'>
+                                        <h1 className='p-1 text-xl font-bold text-white'>Create H544 Request</h1>
+                                    </div>
+                                    <p className='py-1 px-10 text-md font-normal text-white'>Start Campain From H544</p>
+                                </button>
+                            </div>
+                        </div>
+                    
+                        <div className='flex flex-col md:flex-row items-center text-center'>
+                            <button
+                                onClick=''
+                                className='md:m-4 m-4 rounded-3xl border-2 border-emerald-500 md:w-1/2 w-11/12 h-30 shadow-2xl transition duration-700 ease-in-out hover:scale-105'
+                            >
+                                <div className='flex-shrink-0 flex items-center justify-center p-3'>
+                                    <h1 className='p-1 text-xl font-bold text-gray-600'>Check Sent H544 Requests</h1>
+                                </div>
+                                <p className='py-1 px-10 text-md font-normal text-gray-600'>Check Pending Approved H544</p>
+                            </button>
+                            
+                            <button className='md:m-4 m-4 rounded-3xl border-2 border-emerald-500 md:w-1/2 w-11/12 h-30 shadow-2xl transition duration-700 ease-in-out hover:scale-105'>
+                                <div className='flex-shrink-0 flex items-center justify-center p-3'>
+                                    <h1 className='p-1 text-xl font-bold text-gray-600'>Infectious Diseases Register</h1>
+                                </div>
+                                <p className='py-1 px-10 text-md font-normal text-gray-600'>Check Infectious Diseases Register</p>
+                            </button>
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <div className='flex flex-col md:flex-row items-center text-center'>
+                            <button className='md:m-4 m-4 rounded-3xl border-2 border-emerald-500 md:w-1/2 w-11/12 h-30 shadow-2xl transition duration-700 ease-in-out hover:scale-105'>
+                                <div className='flex-shrink-0 flex items-center justify-center p-3'>
+                                    <h1 className='p-1 text-xl font-bold text-gray-600'>View Document Inward</h1>
+                                </div>
+                                <p className='py-1 px-10 text-md font-normal text-gray-600'>Check Inward Register</p>
+                            </button>
+
+                            <button className='md:m-4 m-4 rounded-3xl border-2 border-emerald-500 md:w-1/2 w-11/12 h-30 shadow-2xl transition duration-700 ease-in-out hover:scale-105'>
+                                <div className='flex-shrink-0 flex items-center justify-center p-3'>
+                                    <h1 className='p-1 text-xl font-bold text-gray-600'>View Document Outward</h1>
+                                </div>
+                                <p className='py-1 px-10 text-md font-normal text-gray-600'>Check Outward Register</p>
+                            </button>
+                        </div>
+                    </div>
+                </div> */}
+
+                {/* Main content section PHI*/}
                 <div className="md:w-1/2 p-12 text-center md:text-left" id="PHI">
                     <div>
-                        {/* Button to continue campaigns */}
                         <div>
                             <div className='flex flex-col md:flex-row items-center text-center'>
                                 <button
@@ -32,8 +87,7 @@ export default function Landing() {
                                 </button>
                             </div>
                         </div>
-
-                        {/* Button to check received H544 requests */}
+                    
                         <div className='flex flex-col md:flex-row items-center text-center'>
                             <button
                                 onClick={handleShowH544Table}
@@ -44,8 +98,7 @@ export default function Landing() {
                                 </div>
                                 <p className='py-1 px-10 text-md font-normal text-gray-600'>Check received H544 from MOH</p>
                             </button>
-
-                            {/* Other buttons (Infectious Diseases Register, Inward, Outward etc.) */}
+                            
                             <button className='md:m-4 m-4 rounded-3xl border-2 border-emerald-500 md:w-1/2 w-11/12 h-30 shadow-2xl transition duration-700 ease-in-out hover:scale-105'>
                                 <div className='flex-shrink-0 flex items-center justify-center p-3'>
                                     <h1 className='p-1 text-xl font-bold text-gray-600'>Infectious Diseases Register</h1>
@@ -54,8 +107,7 @@ export default function Landing() {
                             </button>
                         </div>
                     </div>
-
-                    {/* Other sections */}
+                    
                     <div>
                         <div className='flex flex-col md:flex-row items-center text-center'>
                             <button className='md:m-4 m-4 rounded-3xl border-2 border-emerald-500 md:w-1/2 w-11/12 h-30 shadow-2xl transition duration-700 ease-in-out hover:scale-105'>
