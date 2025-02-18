abae0349a7a299705534439f573595ceb0e9e5e0
ksdkaveesha <ksdkaveesha@gmail.com>
2025-02-18 21:18:22 +0530

    dashboard

--- src/Components/Dashboard/LandingApp.jsx
+++ src/Components/Dashboard/LandingApp.jsx
@@ -2,8 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import H544Table from './PHI/H544Table';
-import AcceptedRequestsTable from './PHI/AcceptedRequestsTable';
+
 
 
 function LandingApp() {
@@ -12,9 +11,7 @@ function LandingApp() {
       <div className="App">
         <Routes>
             
-          <Route path="/h544-table" element={<H544Table />} />
-          <Route path="/accepted-requests" element={<AcceptedRequestsTable />} />
-          <Route path="/inward-form/:patientId" element={<InwardForm />} />
+          
         </Routes>
       </div>
     </Router>
