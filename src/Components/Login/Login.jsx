5af9cbf7f4044384da6703cfb4db8e7df827277b
Codersauruse <shehandesk123@gmail.com>
2025-02-20 22:10:53 +0530

    changed the role acessing in authlayout

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -41,17 +41,16 @@ const Login = () => {
       return;
     }
     setLoading(true);
-   
+
     try {
-     
       const response = await authService.login(
         formInput.email,
         formInput.password
       );
-      login(response.data);
-  
+      login(response);
+
       setUser(response);
-      
+
       toast.success("welcome back!");
       navigate("/dashboard");
     } catch (error) {
@@ -125,7 +124,7 @@ const Login = () => {
             </div>
             <div className="w-full px-4 flex justify-center items-center">
               <p>
-                Don't have an account?{" "}
+                Dont have an account?{" "}
                 <a href="/sign-up" className="font-bold">
                   Sign Up
                 </a>
