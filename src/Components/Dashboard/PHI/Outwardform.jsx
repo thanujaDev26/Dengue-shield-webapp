39e355e513ffb40e3262346a04aa247ee8e5aa76
ksdkaveesha <ksdkaveesha@gmail.com>
2025-02-21 12:13:15 +0530

    Update Outwardform.jsx

--- src/Components/Dashboard/PHI/Outwardform.jsx
+++ src/Components/Dashboard/PHI/Outwardform.jsx
@@ -26,6 +26,14 @@ export default function Outwardform({ onSubmit }) {
     onSubmit(formData);
   };
 
+  const handleContinue = () => {
+    navigate('/h411-form'); // Navigate to Notebook with patientId
+  };
+
+  const handleBack = () => {
+    navigate(-1); // Navigate back to the previous page
+  };
+
   return (
     <form className="space-y-12 w-full max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md mt-6 mb-10">
       <h2 className="text-2xl font-semibold text-gray-900">Outward Form</h2>
@@ -37,7 +45,7 @@ export default function Outwardform({ onSubmit }) {
             htmlFor="no"
             className="block text-sm font-medium text-gray-900"
           >
-            NO
+            H544 ID
           </label>
           <input
             type="text"
@@ -46,10 +54,11 @@ export default function Outwardform({ onSubmit }) {
             onChange={handleChange}
             name="no"
             className="w-full p-3 mb-4 border border-gray-300 rounded"
+            disabled
           />
         </div>
 
-        {/* Date Field */}
+        {/* Date Field
         <div className="sm:col-span-3">
           <label
             htmlFor="date"
@@ -65,7 +74,7 @@ export default function Outwardform({ onSubmit }) {
             name="date"
             className="w-full p-3 mb-4 border border-gray-300 rounded"
           />
-        </div>
+        </div> */}
 
         {/* To Whom Field */}
         <div className="sm:col-span-3">
@@ -83,6 +92,7 @@ export default function Outwardform({ onSubmit }) {
             onChange={handleChange}
             name="toWhom"
             className="w-full p-3 mb-4 border border-gray-300 rounded"
+            disabled
           />
         </div>
 
@@ -102,10 +112,11 @@ export default function Outwardform({ onSubmit }) {
             onChange={handleChange}
             name="subject"
             className="w-full p-3 mb-4 border border-gray-300 rounded"
+            disabled
           />
         </div>
 
-        {/* Date of Answer Field */}
+        {/* Date of Answer Field
         <div className="sm:col-span-3">
           <label
             htmlFor="dateOfAnswer"
@@ -121,7 +132,7 @@ export default function Outwardform({ onSubmit }) {
             name="dateOfAnswer"
             className="w-full p-3 mb-4 border border-gray-300 rounded"
           />
-        </div>
+        </div> */}
 
         {/* Remarks Field */}
         <div className="sm:col-span-3">
@@ -144,14 +155,30 @@ export default function Outwardform({ onSubmit }) {
       </div>
 
       {/* Submit Button */}
-      <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button
-          onClick={handleSubmit}
-          className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600"
-        >
-          Submit Outward Form
-        </button>
-      </div>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4 ml-10 mr-10'>
+          <button
+            onClick={handleBack}
+            className="bg-green-500 text-white mt-5 px-4 py-2 rounded-lg hover:bg-green-600"
+          >
+            Back
+          </button>
+          <button
+            onClick={handleSubmit}
+            className="bg-green-500 text-white mt-5 px-4 py-2 rounded-lg hover:bg-green-600"
+          >
+            Submit 
+          </button>
+          <button
+            onClick={handleContinue}
+            className="bg-green-500 text-white mt-5 px-4 py-2 rounded-lg hover:bg-green-600"
+          >
+            Next
+          </button>
+        </div>
     </form>
+
+    
   );
+
+  
 }
